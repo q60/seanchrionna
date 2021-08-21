491ea75d8d18ec3c367003860418b2f67695eee4
llathasa-veleth <llathasa@outlook.com>
2021-08-21 13:08:12 +0300

    D code style. 0.1.1

--- source/app.d
+++ source/app.d
@@ -1,19 +1,18 @@
-import std.string: strip, wrap;
-import std.conv: to;
-import std.stdio: writefln;
-import std.format: format;
-import std.net.curl: get;
+import std.string : strip, wrap;
+import std.conv : to;
+import std.stdio : writefln;
+import std.format : format;
+import std.net.curl : get;
 import dxml.dom;
 
-void main() {
+void main()
+{
     string uri = "https://api.forismatic.com/api/1.0/?" ~
                  "method=getQuote&format=xml&lang=en";
     char[] response = get(uri);
 
     auto dom = parseDOM(response);
-    auto quote = dom
-        .children[0]  // <forismatic>
-        .children[0]; // <quote>
+    auto quote = dom.children[0].children[0];
     string quoteText = to!string(
         quote
             .children[0]
@@ -26,7 +25,8 @@ void main() {
         strip(wrap(quoteText, 60, null, " ")),
     );
 
-    if (quote.children[1].children.length != 0) {
+    if (quote.children[1].children.length != 0)
+    {
         char[] quoteAuthor = quote
             .children[1]
             .children[0]
